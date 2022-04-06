b384db054a77d34b32e62f517995cfa17f2ea0fc
Dmitri Pervouchine <pervouchine@gmail.com>
2022-04-06 18:03:50 +0300

    fly

---
+++ fly/dm3/setup.mk
@@ -0,0 +1,4 @@
+GENOME=https://hgdownload.soe.ucsc.edu/goldenPath/dm3/bigZips/dm3.fa.gz
+ANNOT=http://ftp.flybase.net/releases/FB2006_01/dmel_r5.1/gff/dmel-all-r5.1.gff.gz
+REFSEQ=https://hgdownload.soe.ucsc.edu/goldenPath/dm3/database/refGene.txt.gz
+SCRIPTS=../../scripts/
---
+++ fly/dm6/setup.mk
@@ -0,0 +1,4 @@
+GENOME=https://hgdownload.soe.ucsc.edu/goldenPath/dm6/bigZips/dm6.fa.gz
+ANNOT=http://ftp.ensembl.org/pub/release-104/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP6.32.104.gtf.gz
+REFSEQ=https://hgdownload.soe.ucsc.edu/goldenPath/dm6/database/refGene.txt.gz
+SCRIPTS=../../scripts/
