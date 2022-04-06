22ab82d691aaf7867f82393fd53dbff531e26489
Dmitri Pervouchine <pervouchine@gmail.com>
2022-04-06 17:51:19 +0300

    human

---
+++ human/hg19/setup.mk
@@ -0,0 +1,4 @@
+GENOME=https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
+ANNOT=http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz
+REFSEQ=https://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/refGene.txt.gz
+SCRIPTS=../../scripts/
---
+++ human/hg38/setup.mk
@@ -0,0 +1,5 @@
+GENOME=https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+ANNOT=http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/gencode.v38.annotation.gtf.gz
+REFSEQ=https://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/refGene.txt.gz
+SCRIPTS=../../scripts/
+
